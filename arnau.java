3aa33b8bfbe37c3ca49318bf8fef0ed8c93a270a
aroig3 <101899182+aroig3@users.noreply.github.com>
2022-03-25 18:50:54 +0100

    Update arnau.java

--- arnau.java
+++ arnau.java
@@ -1,18 +1,5 @@
-package primer_java;
-
-public class Alumne {
-public String nom;
-
-public void saluda() {
-   System.out.println("Hola, em dic " + nom);
-}
-
-public Alumne(String nom) {
-   this.nom=nom;
-}
-
-public static void main(String[] args) {
-   Alumne arnau=new Alumne("arnau");
-   arnau.saluda();
-}
+public class arnau {
+   public static void main(String[] args) {
+      System.out.println("Hola Mundo");
+   }
 }
